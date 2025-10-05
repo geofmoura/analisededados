123c7f7e5fd145ee77775ecc194d1be65bafc645
leandrosantino <leandrosantino2013@gmail.com>
2025-10-05 13:45:17 -0300

    Configurado ambiente com uv e Criado teste inicial da análise

---
+++ src/exchange.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+months = [
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+]
+
+def process_exchange_data():
+    df = pd.read_json('data/exchange.json')
+    df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y')
+    df['ano'] = df['data'].dt.year
+    df['mes'] = df['data'].dt.month
+    df['mes'] = df['mes'].apply(lambda x: months[x - 1])
+    df['valor'] = pd.to_numeric(df['valor'], errors='coerce')
+
+    grouped = df.groupby(['ano', 'mes'])['valor'].mean().reset_index()
+    
+    return grouped
+
+
+if __name__ == "__main__":
+    data = process_exchange_data()
+    print(data)
