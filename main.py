cc24f68575e96cae27e93de4948ebf160723249c
leandrosantino <leandrosantino2013@gmail.com>
2025-11-05 22:48:04 -0300

    feat: Adiciona codigo para agupar os dados de comex em um arquivo .xlsx

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.exchange import process_exchange_data
 import os
 from src.comexstat import process_comex_data
 from src.utils import calculateTime, get_logger
-
+from src.generate_report import generate_report_df
 logger = get_logger()
 
 start_time = time.time()
@@ -22,15 +22,21 @@ logger.debug(f"DataFrame 'imports' processado com {len(imports)} linhas.")
 logger.debug(f"DataFrame 'exchange' processado com {len(exchange)} linhas")
 
 logger.info("Salvando dados do ComexStat no banco sqlite")
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect(':memory:')
 
 ncm_isic.to_sql('ncm_isic', connection, if_exists='replace', index=False)
 pais_bloco.to_sql('pais_bloco', connection, if_exists='replace', index=False)
 exports.to_sql('exports', connection, if_exists='replace', index=False)
 imports.to_sql('imports', connection, if_exists='replace', index=False)
 
+logger.info("Salvando arquivo excel com os dados agrupados de Comex")
+report = generate_report_df(connection)
+report.to_excel('data/comexstat.xlsx', index=False, sheet_name='comexstat')
+
 logger.info("Salvando arquivo excel com os dados de Câmbio")
 exchange.to_excel('data/exchange.xlsx', index=False, sheet_name='excahnge')
+
+
 
 end_time = time.time()
 logger.success(f"Algoritmo Finalizado com sucesso em {calculateTime(end_time, start_time)}")
