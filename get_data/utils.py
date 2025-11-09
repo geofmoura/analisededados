7169351d2373d310a563bd96a4dc4babacc46004
leandrosantino <leandrosantino2013@gmail.com>
2025-11-09 15:26:27 -0300

    feat: Gerado os gráficos de secção

--- get_data/utils.py
+++ get_data/utils.py
@@ -1,7 +1,6 @@
 import sys
 from loguru import logger
 
-
 def calculateTime(end_time: float, start_time: float):
     elapsed_time = end_time - start_time
     if elapsed_time > 60:
@@ -12,3 +11,8 @@ def get_logger():
     logger.remove()
     logger.add(sys.stdout, level="DEBUG", format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <level>{message}</level>", colorize=True, enqueue=True)
     return logger
+
+def format_usd_mi(valor: float) -> str:
+    valor_mi = valor / 1_000_000_000
+    valor_formatado = f"{valor_mi:,.1f}".replace(",", "X").replace(".", ",").replace("X", ".")
+    return f"$ {valor_formatado} Bi"
--- plots/plots_pie_charts_by_class.py
+++ plots/plots_pie_charts_by_class.py
@@ -1,44 +1,70 @@
 import sqlite3
-import pandas as pd
+
 import matplotlib.pyplot as plt
+import pandas as pd
+
 from get_data.get_comexstat_data import get_comexstat_data
+from get_data.utils import format_usd_mi
 
-pd.set_option('display.max_columns', None)
+pd.set_option("display.max_columns", None)
 
-def plots_pie_charts_by_class():
-    connection = sqlite3.connect('data/database.db')
-    df = get_comexstat_data(connection)
-
-    df.columns = df.columns.str.lower()
-
+def imports_chat(df):
     # Agora filtramos importação ou exportação normalmente
-    df = df[df['fluxo'] == 'Exportação']  #Importaçao ou Exportação (exatamente como está escrito)
-
-    # Filtra o período desejado
-    df = df[(df['ano'] >= 2017) & (df['ano'] <= 2024)]
+    df = df[df["fluxo"] == "Importaçao"]  # Importaçao ou Exportação (exatamente como está escrito)
 
     # Agrupa por seção isic e soma o valor FOB
     resumo = (
-        df.groupby('secao_isic')['valor_fob_dolar']
+        df.groupby("secao_isic")["valor_fob_dolar"]
         .sum()
         .reset_index()
-        .sort_values(by='valor_fob_dolar', ascending=False)
+        .sort_values(by="valor_fob_dolar", ascending=False)
     )
 
     print(resumo)
 
-    if resumo.empty:
-        print("⚠ Não há dados de importação no período selecionado.")
-        return
+    for index, row in resumo.iterrows():
+        print(format_usd_mi(row['valor_fob_dolar']))
 
     # === GRÁFICO DE PIZZA ===
     plt.figure(figsize=(8, 6))
-    plt.pie(resumo['valor_fob_dolar'], labels=resumo['secao_isic'], autopct='%1.1f%%')
-    plt.title('Participação das exportações por Seção ISIC (2017–2024)')
+    plt.pie(resumo["valor_fob_dolar"].to_list(), labels=resumo["secao_isic"].to_list(), autopct="%1.1f%%")
+    plt.title("Participação das importações por Seção ISIC (2017–2024)")
     plt.tight_layout()
+
+def exports_chat(df):
+    # Agora filtramos importação ou exportação normalmente
+    df = df[df["fluxo"] == "Exportação"]  # Importaçao ou Exportação (exatamente como está escrito)
+
+    # Agrupa por seção isic e soma o valor FOB
+    resumo = (
+        df.groupby("secao_isic")["valor_fob_dolar"]
+        .sum()
+        .reset_index()
+        .sort_values(by="valor_fob_dolar", ascending=False)
+    )
+
+    print(resumo)
+
+    for index, row in resumo.iterrows():
+        print(format_usd_mi(row['valor_fob_dolar']))
+
+    # === GRÁFICO DE PIZZA ===
+    plt.figure(figsize=(8, 6))
+    plt.pie(resumo["valor_fob_dolar"].to_list(), labels=resumo["secao_isic"].to_list(), autopct="%1.1f%%")
+    plt.title("Participação das exportações por Seção ISIC (2017–2024)")
+    plt.tight_layout()
+
+def plots_pie_charts_by_class():
+    connection = sqlite3.connect("data/database.db")
+    df = get_comexstat_data(connection)
+
+    df.columns = df.columns.str.lower()
+    imports_chat(df)
+    exports_chat(df)
+
     plt.show()
 
-    return resumo
+
 
 
 if __name__ == "__main__":
